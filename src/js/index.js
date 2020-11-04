bde4b3bda41e1fa72dfb0df168c6e35c95773df9
Michelle Barker <mbarker_84@hotmail.co.uk>
2020-11-04 09:58:20 +0000

    Overwrite service worker

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,20 @@
 import menu from './menu'
 import headerChange from './header'
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      // registration.unregister().then(function(boolean) {
+      //   console.log(boolean == true ? 'unregistered' : 'failed to unregister')
+      // });
+    }, function(err) {
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
+
 menu()
 headerChange()
 
