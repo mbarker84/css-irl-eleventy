f1a48bcc8de5ddead1ef85e0ea14d631f4348495
Michelle Barker <mbarker_84@hotmail.co.uk>
2020-11-10 22:14:24 +0000

    Delete caches

--- src/js/service-worker.js
+++ src/js/service-worker.js
@@ -3,8 +3,17 @@ self.addEventListener('install', function(e) {
 });
 
 self.addEventListener('activate', function(e) {
+  self.caches.keys()
+    .then(keys => keys.forEach(key => {
+      console.log(key)
+      self.caches.delete(key)
+    }))
+
+  console.log(self.caches)
+
   self.registration.unregister()
     .then(function() {
+
       return self.clients.matchAll();
     })
     .then(function(clients) {
