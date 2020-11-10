ac7656b2fd763defbf71a7b2cd8b9428c884f70c
Michelle Barker <mbarker_84@hotmail.co.uk>
2020-11-10 21:29:33 +0000

    Unregister all service workers

--- src/js/index.js
+++ src/js/index.js
@@ -3,19 +3,18 @@ import headerChange from './header'
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      // registration.unregister().then(function(boolean) {
-      //   console.log(boolean == true ? 'unregistered' : 'failed to unregister')
-      // });
-    }, function(err) {
-      console.log('ServiceWorker registration failed: ', err);
-    });
-    // navigator.serviceWorker.getRegistrations().then( function(registrations) {
-    //   for(let registration of registrations) {
-    //     registration.unregister();
-    //   }
+    // navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+    //   console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    // }, function(err) {
+    //   console.log('ServiceWorker registration failed: ', err);
     // });
+    navigator.serviceWorker.getRegistrations().then( function(registrations) {
+      for(let registration of registrations) {
+        registration.unregister().then((unregistered) => {
+          console.log(unregistered == true ? 'unregistered' : 'failed to unregister')
+        })
+      }
+    });
   });
 }
 
