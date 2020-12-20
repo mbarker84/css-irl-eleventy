e5564bb724d41eb25ea6c00f6fb88d4ff72f78aa
Michelle Barker <mbarker_84@hotmail.co.uk>
2020-12-20 11:22:41 +0000

    ES Lint, Stylelint and Prettier configuration

--- src/js/index.js
+++ src/js/index.js
@@ -2,23 +2,21 @@ import menu from './menu'
 import headerChange from './header'
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      console.log('ServiceWorker registration failed: ', err);
-    });
-    // navigator.serviceWorker.getRegistrations().then( function(registrations) {
-    //   for(let registration of registrations) {
-    //     registration.unregister().then((unregistered) => {
-    //       console.log(unregistered == true ? 'unregistered' : 'failed to unregister')
-    //     })
-    //   }
-    // });
-  });
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('/service-worker.js').then(
+      function (registration) {
+        console.log(
+          'ServiceWorker registration successful with scope: ',
+          registration.scope
+        )
+      },
+      function (err) {
+        console.log('ServiceWorker registration failed: ', err)
+      }
+    )
+  })
 }
 
 document.body.classList.remove('no-js')
 menu()
 headerChange()
-
