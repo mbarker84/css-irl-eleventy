be0ad42bedc887293877b0473d89d01252a5a0ba
Michelle Barker <mbarker_84@hotmail.co.uk>
2021-11-11 16:02:42 +0000

    Upgrade to Parcel 2 (WIP)

--- src/js/index.js
+++ src/js/index.js
@@ -11,17 +11,20 @@ const components = [
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function () {
-    navigator.serviceWorker.register('/service-worker.js').then(
-      function (registration) {
-        console.log(
-          'ServiceWorker registration successful with scope: ',
-          registration.scope
-        )
-      },
-      function (err) {
-        console.log('ServiceWorker registration failed: ', err)
-      }
-    )
+    navigator.serviceWorker.register(new URL('service-worker.js'), {
+      type: 'module',
+    })
+    // navigator.serviceWorker.register('/service-worker.js').then(
+    //   function (registration) {
+    //     console.log(
+    //       'ServiceWorker registration successful with scope: ',
+    //       registration.scope
+    //     )
+    //   },
+    //   function (err) {
+    //     console.log('ServiceWorker registration failed: ', err)
+    //   }
+    // )
   })
 }
 
